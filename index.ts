f57dea71b9ea17304b5d6bb9065ef5aa8e1793bb
joaovittormkt <joaovittorvieira15@gmail.com>
2025-06-30 13:48:48 -0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação tipada
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+// Função de saudação tipada
+function saudacao(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+// Testes no console
+console.log(multiplicar(4, 5));     // 20
+console.log(saudacao("Flávia"));   // Olá Flávia
